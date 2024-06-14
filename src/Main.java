aa2e206cbfa848e83970e09fb93958cefa5c79a2
Filip Žitný <88278380+Yggdrasill501@users.noreply.github.com>
2024-06-14 15:14:41 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         String password = "supersecret123";
-        String apiKey = "12345-adbcde-67890-fghij";
-        String token = "AKKAisf59323423jExamPLe";
+        String apiKey = "12345-abcde-67890-fghij";
+        String awsAccessKeyId = "AKIAIOSFODNN7EXAMPLE";
+        String awsSecretKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+        String dbConnectionString = "jdbc:mysql://username:password@localhost:3306/mydb";
 
-        String publicPassword = "This wil note be ingored check .gileaks.toml"
-
-        System.out.println("This is just Example don't worry");
+        System.out.println("Sensitive information for testing Gitleaks:");
+        System.out.println("Password: " + password);
+        System.out.println("API Key: " + apiKey);
+        System.out.println("AWS Access Key ID: " + awsAccessKeyId);
+        System.out.println("AWS Secret Key: " + awsSecretKey);
+        System.out.println("DB Connection String: " + dbConnectionString);
     }
 }
