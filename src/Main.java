775b1ff40bc1517ab94e6e003d209a9531d46acd
Filip Žitný <88278380+Yggdrasill501@users.noreply.github.com>
2024-06-14 15:09:32 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        String password = "supersecret123";
+        String apiKey = "12345-adbcde-67890-fghij";
+        String token = "AKKAisf59323423jExamPLe";
 
+        String publicPassword = "This wil note be ingored check .gileaks.toml"
+
+        System.out.println("This is just Example don't worry");
+    }
+}
